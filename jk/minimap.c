c51bd7ef0502979a6d0dcb26cacc37d346156df8
Jeein Kim <jeekim@c3r4s7.42seoul.kr>
2022-12-29 21:09:50 +0900

    minimap.c ing

--- jk/minimap.c
+++ jk/minimap.c
@@ -1,12 +1,12 @@
 #include "cub3d.h"
 
-void fill_square(t_all *p_all, int x, int y, int color)
+void	fill_square(t_all *p_all, int x, int y, int color)
 {
-	int j;
-	int k;
-	t_point	p;
+	int			j;
+	int			k;
+	t_point_db	p;
 
-	set_point(&p, x, y);
+	set_point_db(&p, x, y);
 	locate_for_mini(&x, &y, p);
 	k = 0;
 	while (k <= (int)(MINI_SCALE * p_all->map.row_tile_size))
@@ -21,7 +21,7 @@ void fill_square(t_all *p_all, int x, int y, int color)
 	}
 }
 
-void render_map(t_all *p_all)
+void	render_map(t_all *p_all)
 {
 	unsigned long long	j;
 	unsigned long long	k;
@@ -34,9 +34,15 @@ void render_map(t_all *p_all)
 		while (j < p_all->map.col)
 		{
 			if (p_all->map.dp_map[k][j] == '1')
-				fill_square(p_all, (int)(p_all->map.col_tile_size * j), (int)(p_all->map.row_tile_size * k), BLACK);
+				fill_square(p_all,
+					(int)(p_all->map.col_tile_size * j),
+					(int)(p_all->map.row_tile_size * k),
+					BLACK);
 			else if (ft_strchr("0NSEW",p_all->map.dp_map[k][j]))
-				fill_square(p_all, (int)(p_all->map.col_tile_size * j), (int)(p_all->map.row_tile_size * k), WHITE);
+				fill_square(p_all,
+					(int)(p_all->map.col_tile_size * j),
+					(int)(p_all->map.row_tile_size * k),
+					WHITE);
 			else
 				;
 			j++;
