f9e76e52b01962366243c99582e02df69e85e3c5
Jeein Kim <jeekim@c3r4s7.42seoul.kr>
2022-12-29 22:57:43 +0900

    update_player() 25줄 이하 만드는중.. 이 파일에선 이 함수만 다듬으면 돼

--- jk/player_main.c
+++ jk/player_main.c
@@ -1,65 +1,68 @@
 #include "cub3d.h"
 
-void update_player(t_all *p_all)
+void	update_player2(t_all *p_all, t_point_db	*p_old_player, t_point_db	*p_new_player)
 {
-	double turnDirection;
-	double walkDirection;
-	double newPlayerX;
-	double newPlayerY;
-	double	move_side;
-	t_point_db	p1;
-	t_point_db	p2;
-
-	turnDirection = 0;
-	walkDirection = 0;
-	move_side = 0;
-	if (p_all->key.down)
-		walkDirection = -1;
-	if (p_all->key.up)
-		walkDirection = 1;
-	if (p_all->key.right)
+	if (!hit_wall((*p_new_player).x, (*p_new_player).y, p_all) && !check_edge(p_all, *p_old_player, *p_new_player))
 	{
-		walkDirection = -1;
-		move_side = M_PI_2;
-	}
-	if (p_all->key.left)
-	{
-		walkDirection = 1;
-		move_side = M_PI / 2.0;
-	}
-	if (p_all->key.right_rotation)
-		turnDirection = 1;
-	if (p_all->key.left_rotation)
-		turnDirection = -1;
-
-	p_all->player.rotation_angle += turnDirection * p_all->player.turn_speed;
-	int moveStep;
-	moveStep = walkDirection * p_all->player.walk_speed;
-	newPlayerX = p_all->player.x + moveStep * cos(p_all->player.rotation_angle - move_side);
-	newPlayerY = p_all->player.y + moveStep * sin(p_all->player.rotation_angle - move_side);
-	set_point_db(&p1, p_all->player.x, p_all->player.y);
-	set_point_db(&p2, newPlayerX, newPlayerY);
-	if (!hit_wall(newPlayerX, newPlayerY, p_all) && !check_edge(p_all, p1, p2))
-	{
-		p_all->player.x = newPlayerX;
-		p_all->player.y = newPlayerY;
+		p_all->player.x = (*p_new_player).x;
+		p_all->player.y = (*p_new_player).y;
 	}
 }
 
-void draw_player(t_all *p_all)
+double	if_true_change_val(bool true_false, int set_val, double ret)
 {
-	int		x;
-	int		y;
+	if (true_false == true)
+		return (set_val);
+	else
+		return (ret);
+}
+
+void	update_player(t_all *p_all)
+{
+	t_point_db	dir;
+	t_point_db	new_player;
+	t_point_db	old_player;
+	t_point_db	mv;
+
+	set_point_db(&dir, 0, 0);
+	set_point_db(&mv, 0, (int)(dir.x * p_all->player.walk_speed));
+	dir.x = if_true_change_val(p_all->key.down, -1, dir.x);
+	dir.x = if_true_change_val(p_all->key.up, 1, dir.x);
+	if (p_all->key.right)
+	{
+		dir.x = -1;
+		mv.x = M_PI_2;
+	}
+	if (p_all->key.left)
+	{
+		dir.x = 1;
+		mv.x = M_PI / 2.0;
+	}
+	dir.y = if_true_change_val(p_all->key.right_rotation, 1, dir.y);
+	dir.y = if_true_change_val(p_all->key.left_rotation, -1, dir.y);
+	p_all->player.rotation_angle += dir.y * p_all->player.turn_speed;
+	set_point_db(&mv, mv.x, (int)(dir.x * p_all->player.walk_speed));
+	set_point_db(&old_player, p_all->player.x, p_all->player.y);
+	set_point_db(&new_player,
+		old_player.x + (int)(mv.y) *cos(p_all->player.rotation_angle - mv.x),
+		old_player.y + (int)(mv.y) *sin(p_all->player.rotation_angle - mv.x));
+	update_player2(p_all, &old_player, &new_player);
+}
+
+void	draw_player(t_all *p_all)
+{
+	int			x;
+	int			y;
 	t_point_db	p;
+	int			row;
+	int			col;
 
 	set_point_db(&p, p_all->player.x, p_all->player.y);
 	locate_for_mini(&x, &y, p);
 	update_player(p_all);
-	int	row;
 	row = (-(MINI_SCALE * p_all->map.row_tile_size) / 10);
 	while (row <= (MINI_SCALE * p_all->map.row_tile_size / 10))
 	{
-		int	col;
 		col = (-(MINI_SCALE * p_all->map.col_tile_size) / 10);
 		while (col <= (MINI_SCALE * p_all->map.col_tile_size / 10))
 		{
