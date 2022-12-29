94b68da2543749b0b4850e39fd373813a88701d9
Jeein Kim <jeekim@c3r4s7.42seoul.kr>
2022-12-29 20:58:11 +0900

    init2.c norm 완

--- jk/init2.c
+++ jk/init2.c
@@ -1,15 +1,16 @@
 #include "cub3d.h"
 
-void ray_init(t_ray *p_ray, double ang)
+void	ray_init(t_ray *p_ray, double ang)
 {
 	p_ray->ray_angle = normalize_fisheye(ang);
 	p_ray->xhit_wall = 0;
 	p_ray->yhit_wall = 0;
 	p_ray->distance = 0;
 	p_ray->vert_hit = false;
-	p_ray->ray_faces_down = p_ray->ray_angle > 0 && p_ray->ray_angle < M_PI;
+	p_ray->ray_faces_down = (p_ray->ray_angle > 0 && p_ray->ray_angle < M_PI);
 	p_ray->ray_faces_up = !(p_ray->ray_faces_down);
-	p_ray->ray_faces_right = p_ray->ray_angle < 0.5 * M_PI || p_ray->ray_angle > 1.5 *  M_PI;
+	p_ray->ray_faces_right
+		= (p_ray->ray_angle < 0.5 * M_PI || p_ray->ray_angle > 1.5 * M_PI);
 	p_ray->ray_faces_left = !(p_ray->ray_faces_right);
 }
 
