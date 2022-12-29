23186e9ab79cf5700275248ee4e8f59c3aa25ecf
Jeein Kim <jeekim@c3r4s7.42seoul.kr>
2022-12-29 20:55:55 +0900

    init.c norminette 맞춤

--- jk/init.c
+++ jk/init.c
@@ -1,6 +1,6 @@
 #include "cub3d.h"
 
-void init_img_data(t_all* p_all) 
+void	init_img_data(t_all *p_all)
 {
 	int	x;
 	int	y;
@@ -18,13 +18,13 @@ void init_img_data(t_all* p_all)
 	}
 }
 
-void init_player(t_all *p_all)
+void	init_player(t_all *p_all)
 {
 	p_all->player.x = WINDOW_WID / 2;
 	p_all->player.y = WINDOW_HEI / 2;
-	p_all->player.rotation_angle = 0;// E일 경우.오른쪽을 봄
+	p_all->player.rotation_angle = 0;
 	p_all->player.walk_speed = 1;
-	p_all->player.turn_speed = 2 * (M_PI / 180);//?
+	p_all->player.turn_speed = 2 * (M_PI / 180);
 	p_all->player.fov_angle = M_PI / 3;
 }
 
@@ -43,8 +43,11 @@ void	init_mlx(t_all *p_all)
 	p_all->mlx = mlx_init();
 	p_all->win = mlx_new_window(p_all->mlx, WINDOW_WID, WINDOW_HEI, "title");
 	p_all->img.img = mlx_new_image(p_all->mlx, WINDOW_WID, WINDOW_HEI);
-	p_all->img.data = (int *)mlx_get_data_addr(p_all->img.img, &(p_all->img.bpp), 
-										&(p_all->img.size_line), &(p_all->img.endian));
+	p_all->img.data
+		= (int *)mlx_get_data_addr(p_all->img.img,
+			&(p_all->img.bpp),
+			&(p_all->img.size_line),
+			&(p_all->img.endian));
 }
 
 void	ft_init(t_all *p_all)
