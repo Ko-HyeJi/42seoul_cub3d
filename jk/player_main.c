72e2abe489f0401950fc6ec8bd6aa086ef83cbe1
Jeein Kim <jeekim@c3r3s7.42seoul.kr>
2022-12-31 15:02:26 +0900

    player_main.c norm 완

--- jk/player_main.c
+++ jk/player_main.c
@@ -1,20 +1,21 @@
 #include "cub3d.h"
 
-double	ft_cos(t_player *p_player, t_point_db *p_mv)
+double	ft_cos(t_all *p_all, t_point_db *p_mv)
 {
-	return (p_player->x + (int)(p_mv->y) * cos(p_player->rotation_angle - p_mv->x));
+	return (p_all->player.x
+		+ (int)(p_mv->y) * cos(p_all->player.rotation_angle - p_mv->x));
 }
 
-double	ft_sin(t_player *p_player, t_point_db *p_mv)
+double	ft_sin(t_all *p_all, t_point_db *p_mv)
 {
-	return (p_player->y + (int)(p_mv->y) * sin(p_player->rotation_angle - p_mv->x));
+	return (p_all->player.y
+		+ (int)(p_mv->y) * sin(p_all->player.rotation_angle - p_mv->x));
 }
 
 void	update_player(t_all *p_all)
 {
 	t_point_db	dir;
 	t_point_db	new_player;
-	t_point_db	old_player;
 	t_point_db	mv;
 
 	set_point_db(&dir, 0, 0);
@@ -35,14 +36,17 @@ void	update_player(t_all *p_all)
 	dir.y = if_true_change_val(p_all->key.left_rotation, -1, dir.y);
 	p_all->player.rotation_angle += dir.y * p_all->player.turn_speed;
 	set_point_db(&mv, mv.x, (int)(dir.x * p_all->player.walk_speed));
-	set_point_db(&old_player, p_all->player.x, p_all->player.y);
-	set_point_db(&new_player, ft_cos(&(p_all->player), &mv), ft_sin(&(p_all->player), &mv));
-	update_player2(p_all, &old_player, &new_player);
+	set_point_db(&new_player, ft_cos(p_all, &mv), ft_sin(p_all, &mv));
+	update_player2(p_all, &new_player);
 }
 
-void	update_player2(t_all *p_all, t_point_db	*p_old_player, t_point_db	*p_new_player)
+void	update_player2(t_all *p_all, t_point_db	*p_new_player)
 {
-	if (!hit_wall((*p_new_player).x, (*p_new_player).y, p_all) && !check_edge(p_all, *p_old_player, *p_new_player))
+	t_point_db	old_player;
+
+	set_point_db(&old_player, p_all->player.x, p_all->player.y);
+	if (!hit_wall((*p_new_player).x, (*p_new_player).y, p_all)
+		&& !check_edge(p_all, old_player, *p_new_player))
 	{
 		p_all->player.x = (*p_new_player).x;
 		p_all->player.y = (*p_new_player).y;
